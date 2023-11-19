e42e3b7ae550df8acc92e185fa17b24236aa57c1
alineking5388 <alineking5388@gmail.com>
2023-11-19 10:13:59 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Camp from "@/Components/Camp";
+import Features from "@/Components/Features";
+import Guide from "@/Components/Guide";
+import Hero from "@/Components/Hero";
+import GetApp from "@/Components/GetApp";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  )
+}
